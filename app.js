5c20e682392671a0a15b0e562b51b5f23386b395
Milena Boselli Rosa <milena.rosa@gmail.com>
2018-10-22 20:14:01 +0000

    add mongolab

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index");
 
 
-mongoose.connect("mongodb://localhost/yelp_camp_v2", {useNewUrlParser: true});
+// mongoose.connect("mongodb://localhost/yelp_camp_v2", {useNewUrlParser: true});
+mongoose.connect("mongodb://boselli:meggie5@ds139523.mlab.com:39523/yelpcamp", {useNewUrlParser: true});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
